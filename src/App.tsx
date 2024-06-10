0f10c0b02943691bf260c296dc1d760c5c36dbea
Levi Ágape <agapelevi99@gmail.com>
2024-06-10 19:22:42 -0300

    feat: Create the homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import { useState } from 'react'
+import ButtonHeader from './components/button' 
+import logo from "./assets/MARCA EVELYN NADINE-03.png"
 import './App.css'
+import "./index.css"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+      <div className="container-all-page">
+        <div className="container-header">
+          <img className="photo-logo-header" src = {logo}></img>
+          <form>
+            <label>Nome</label>
+            <input className="input-header" type="text" placeholder="seueamil@gmail.com"></input>
+          </form>
+          <form>
+          <label>Senha</label>
+            <input className="input-header" placeholder='123' type="password"></input>
+            <ButtonHeader/>
+          </form>
+        </div>
 
-    </>
+      </div>
   )
 }
 
---
+++ src/components/button.tsx
@@ -0,0 +1,8 @@
+function ButtonHeader(){
+    return (
+        <div>
+            <button className="button-header">Teste-me</button>
+        </div>
+    )
+}
+export default ButtonHeader
